bcf52b49550a8e2b7fe7e19c12ef608be5af3302
Foly <92996451+LonelyFoly@users.noreply.github.com>
2022-04-21 22:53:46 +0300

    Новый класс

---
+++ test-bot/Bot v2/user.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotManager
+{
+    internal class botUser
+    {
+        long id;
+
+        internal byte step { get {return step; } set {; } }
+        
+        
+        internal botUser (long id)
+        {
+            this.id = id;
+            step = 1;
+        }
+
+
+
+    }
+}
