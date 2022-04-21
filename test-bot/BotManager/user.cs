b5fcce7171e6c20d390f3ff4a6840470b9b2887f
Foly <92996451+LonelyFoly@users.noreply.github.com>
2022-04-22 00:22:53 +0300

    Доделка #2
    Для банов свойство добавилось

--- test-bot/BotManager/user.cs
+++ test-bot/BotManager/user.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,16 +11,23 @@ namespace BotManager
     {
         long id;
 
+        //5 минут
+        TimeSpan dif = new TimeSpan(0,5,0);
+
         internal byte step;
 
         internal byte tries;
 
         internal DateTime banDate;
 
+        //проверка, в бане ли челик
+        internal bool IsBanned { get {return DateTime.Now.Subtract(banDate).CompareTo(new TimeSpan(0,5*tries/5,0))<=0; } }
+
         internal botUser (long id)
         {
             this.id = id;
             step = 1;
+            tries = 0;
         }
 
 
