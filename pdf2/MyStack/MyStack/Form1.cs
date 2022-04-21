170d6d0b404c80166b7b754659b1b25768e40670
GolubtsovaSof <92886622+GolubtsovaSof@users.noreply.github.com>
2022-04-21 10:56:54 +0300

    стандарный+шаблон

---
+++ pdf2/MyStack/MyStack/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyStack
+{
+	public partial class Form1 : Form
+	{
+		MyStack myStack;
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				myStack = new MyStack(Convert.ToInt32(textBox1.Text));
+			}
+			catch(FormatException)
+			{
+				MessageBox.Show("Set the length correctly!");
+			}
+			textBox1.Clear();
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			try 
+			{
+				myStack.Push(Convert.ToChar(textBox2.Text));
+			}
+			catch(FormatException)
+			{
+				MessageBox.Show("Set new element correctly");
+			}
+			catch
+			{
+				MessageBox.Show("MyStack overflow");
+			}
+			textBox2.Clear();
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				MessageBox.Show(myStack.Pop().ToString()+" will be removed");
+			}
+			catch
+			{
+				MessageBox.Show("Stack is empty");
+			}
+		}
+
+		private void button4_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				MessageBox.Show(myStack.Top().ToString());
+			}
+			catch
+			{
+				MessageBox.Show("Stack is empty");
+			}
+		}
+
+		private void button5_Click(object sender, EventArgs e)
+		{
+			if (myStack.IsEmpty())
+			{
+				MessageBox.Show("MyStack is empty");
+			}
+			else
+			{
+				MessageBox.Show("MyStack isn't empty");
+			}
+		}
+
+		private void button6_Click(object sender, EventArgs e)
+		{
+			myStack.Clear();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+		private string Check()
+		{
+			MyStack s = new MyStack(textBox3.Text.Length / 2);
+			char compare;
+			foreach (char symbol in textBox3.Text)
+			{
+				if (symbol.Equals(Convert.ToChar("{")) || symbol.Equals(Convert.ToChar("(")) || symbol.Equals(Convert.ToChar("["))) s.Push(symbol);
+				if (symbol.Equals(Convert.ToChar("}")) || symbol.Equals(Convert.ToChar(")")) || symbol.Equals(Convert.ToChar("]")))
+				{
+					if (s.IsEmpty()) return ("Incorrect!");
+					else compare = s.Top();
+					if (symbol.Equals(Convert.ToChar("}")) && compare.Equals(Convert.ToChar("{")) || symbol.Equals(Convert.ToChar(")")) && compare.Equals(Convert.ToChar("(")) || symbol.Equals(Convert.ToChar("]")) && compare.Equals(Convert.ToChar("[")))
+					{
+						s.Pop();
+					}
+					else
+					{
+						return("Incorrect!");
+					}
+				}
+			}
+			if (!s.IsEmpty()) return("Incorrect!");
+			else return("Correct!");
+		}
+		private void button7_Click(object sender, EventArgs e)
+		{
+			MessageBox.Show(Check());
+		}
+	}
+}
---
+++ pdf2/PDFreport1/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+
+namespace PDFreport1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+
+			PdfDocument protocol = PdfSharp.Pdf.IO.PdfReader.Open("C:\\Users\\соня\\Desktop\\pdf\\prot2.pdf", PdfSharp.Pdf.IO.PdfDocumentOpenMode.Import); //путь к шаблону с пустыми полями
+			PdfDocument doc = new PdfDocument();
+			doc.AddPage(protocol.Pages[0]);
+			var page = doc.Pages[0];
+
+			XGraphics g = XGraphics.FromPdfPage(page);
+			XFont font = new XFont("Times New Roman", 11);
+			XFont font1 = new XFont("Times New Roman", 11, XFontStyle.Bold);
+			XFont font2 = new XFont("Times New Roman", 12);
+			XFont font3 = new XFont("Times New Roman", 8);
+
+			//1.ОБЩИЕ СВЕДЕНИЯ О СЕАНСЕ
+			g.DrawString("ЗНАЧ", font, XBrushes.Black,
+				new XRect(66, 160.5, 70, 18), XStringFormats.Center); //название организации
+			g.DrawString("ЗНАЧ", font, XBrushes.Black,
+				new XRect(66+70, 160.5, 121, 18), XStringFormats.Center); //Шифр или наименование работы
+			g.DrawString("ЗНАЧ", font, XBrushes.Black,
+				new XRect(66 + 70+121, 160.5, 182, 18), XStringFormats.Center); //Облучаемое изделие
+			g.DrawString("ЗНАЧ", font, XBrushes.Black,
+				new XRect(66 + 70 + 121+182, 160.5, 121, 18), XStringFormats.Center); //Время начала облучения
+			g.DrawString("ЗНАЧ", font, XBrushes.Black,
+				new XRect(66 + 70 + 121 + 182+121, 160.5, 108, 18), XStringFormats.Center);  //Длительность
+			//2.УСЛОВИЯ ЭКСПЕРИМЕНТА: В СРЕДЕ ВАКУУМ
+			g.DrawString("ЗНАЧ", font, XBrushes.Black,
+				new XRect(66, 224, 70, 17), XStringFormats.Center); //угол
+			g.DrawString("ЗНАЧ", font, XBrushes.Black,
+				new XRect(66+70, 224, 121, 17), XStringFormats.Center); //Температура
+			g.DrawString("ЗНАЧ", font, XBrushes.Black,
+				new XRect(66 + 70+121, 224, 121, 17), XStringFormats.Center); //материал дегрейдора
+			g.DrawString("ЗНАЧ", font, XBrushes.Black,
+				new XRect(66 + 70 + 121*2, 224, 121, 17), XStringFormats.Center); //толщина
+			//3.ХАРАКТЕРИСТИКИ ИОНА
+			g.DrawString("ЗН", font, XBrushes.Black,
+				new XRect(66, 298, 70/2, 17), XStringFormats.CenterRight);//тип иона
+			g.DrawString("000", font3, XBrushes.Black,
+				new XRect(66+70/2, 299, 70/2, 17), XStringFormats.TopLeft);//номер для иона
+			g.DrawString("ЗНАЧ", font, XBrushes.Black,
+				new XRect(66 + 70, 298, 182, 17), XStringFormats.Center); //энергия Е на поверхности
+			g.DrawString("ЗНАЧ", font, XBrushes.Black,
+				new XRect(66 + 70+182, 298, 121, 17), XStringFormats.Center); //пробег R
+			g.DrawString("ЗНАЧ", font, XBrushes.Black,
+				new XRect(66 + 70 + 182+121, 298, 182, 17), XStringFormats.Center); //линейные потери энергии ЛПЭ
+			//4.ДАННЫЕ ПО ПРОПОРЦИОНАЛЬНЫМ СЧЕТЧИКАМ
+			g.DrawString("ЗНАЧ", font, XBrushes.Black,
+				new XRect(66, 363, 70, 26), XStringFormats.Center); //1
+			g.DrawString("ЗНАЧ", font, XBrushes.Black,
+				new XRect(66+70, 363, 182/3, 26), XStringFormats.Center); //2
+			g.DrawString("ЗНАЧ", font, XBrushes.Black,
+				new XRect(66 + 70+182/3, 363, 182 / 3, 26), XStringFormats.Center); //3
+			g.DrawString("ЗНАЧ", font, XBrushes.Black,
+				new XRect(66 + 70 + 182 / 3+182/3, 363, 182 / 3, 26), XStringFormats.Center); //4
+			g.DrawString("ЗНАЧ", font, XBrushes.Black,
+				new XRect(66+70+182, 363, 121, 26), XStringFormats.Center); //среднее значение
+			//5.Данные по трековым мембранам из лавсановой пленки:
+			g.DrawString("ЗНАЧ", font, XBrushes.Black,
+				new XRect(66, 427, 70, 26), XStringFormats.Center); //детектор 1
+			g.DrawString("ЗНАЧ", font, XBrushes.Black,
+				new XRect(66 + 70, 427, 182 / 3, 26), XStringFormats.Center); //детектор 2
+			g.DrawString("ЗНАЧ", font, XBrushes.Black,
+				new XRect(66 + 70+182/3, 427, 182 / 3, 26), XStringFormats.Center); //детектор 3
+			g.DrawString("ЗНАЧ", font, XBrushes.Black,
+				new XRect(66 + 70 + 182/3+182/3, 427, 182 / 3, 26), XStringFormats.Center); //детектор 4
+			g.DrawString("ЗНАЧ", font, XBrushes.Black,
+				new XRect(66 + 70 + 182, 427, 121/2, 26), XStringFormats.Center); //детектор 5
+			g.DrawString("ЗНАЧ", font, XBrushes.Black,
+				new XRect(66 + 70 + 182+121/2, 427, 121 / 2, 26), XStringFormats.Center); //детектор 6
+			g.DrawString("ЗНАЧ", font, XBrushes.Black,
+				new XRect(66 + 70 + 182 + 121, 427, 182/3, 26), XStringFormats.Center); //детектор 7
+			g.DrawString("ЗНАЧ", font, XBrushes.Black,
+				new XRect(66 + 70 + 182 + 121+182/3, 427, 182 / 3, 26), XStringFormats.Center); //детектор 8
+			g.DrawString("ЗНАЧ", font, XBrushes.Black,
+				new XRect(66 + 70 + 182 + 121+182/3+182/3, 427, 182 / 3, 26), XStringFormats.Center); //детектор 9
+			g.DrawString("ЗНАЧ", font, XBrushes.Black,
+				new XRect(66 + 70 + 182 + 121 + 182, 427, 153, 26), XStringFormats.Center); //неоднородность
+
+			g.DrawString("000", font1, XBrushes.Black,
+				new XRect(735.5, 98.5, 20, 9.5), XStringFormats.Center); //номер сеанса
+			g.DrawString("000", font2, XBrushes.Black,
+				new XRect(755.5, 80.5, 18, 10), XStringFormats.Center); //номер сеанса в ТЗЧ ляляля
+			g.DrawString("0,00", font, XBrushes.Black,
+				new XRect(669.5, 316.5, 44, 12), XStringFormats.Center); //K
+			g.DrawString("0,00", font, XBrushes.Black,
+				new XRect(733.5, 316.5, 44, 12), XStringFormats.Center); //K погрешность
+			g.DrawString("0/0-0", font, XBrushes.Black,
+				new XRect(659.5, 349, 46.5, 12), XStringFormats.Center); //номер протокола допуска
+			g.DrawString("0,00", font, XBrushes.Black,
+				new XRect(668, 376, 46.5, 12), XStringFormats.Center); //K факт
+
+			doc.Save("C:\\Users\\соня\\Desktop\\pdf\\Test1.pdf"); //путь, куда сохранять док
+		}
+	}
+}
